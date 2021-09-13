b467f1ef7f54f4b2c33b413073e21f5e55795625
DARSHIL SHAH <my.name@example.com>
2021-09-13 10:50:03 +0300

    Final Version

---
+++ app/src/main/java/com/example/internal_externalstorage/fragmentOne.kt
@@ -0,0 +1,90 @@
+package com.example.internal_externalstorage
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import kotlinx.android.synthetic.main.fragment_one.*
+import java.lang.Exception
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [fragmentOne.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class fragmentOne : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        readBtn.setOnClickListener {
+            parentFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<fragmentTwo>(R.id.fragmentContainerView)
+                addToBackStack(null)
+            }
+        }
+        saveBtn.setOnClickListener {
+            writeText()
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FirstFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            fragmentOne().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+    private fun writeText() {
+        try {
+            activity?.openFileOutput(FILENAME, Context.MODE_APPEND).use {
+                it?.write("${editText.text}\n".toByteArray())
+            }
+            editText.text.clear()
+
+        } catch (e: Exception) {
+            e.stackTrace
+        }
+    }
+}
